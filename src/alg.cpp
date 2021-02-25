a952713e028aceded0aa76c8dfa0c5abb7713659
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-02-25 13:30:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,13 +6,13 @@ int gcd(int a, int b) {
     else {
         do {
          if ( a > b ) {
-            a = a - ( a / b ) * b;
+            a = a - (a / b) * b;
              return a;    
             }
             else { 
-             b = b - ( b / a ) * a;
+             b = b - (b / a) * a;
              return b; 
             }
-        } while ( ( a != 0 ) || ( b != 0 ) ) ;
+        } while (( a != 0 ) || ( b != 0 )) ;
     }
 }
